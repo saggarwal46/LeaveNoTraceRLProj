08e3a50224c6680f04a12f2719cb1c8b819cbe4f
Ubuntu <ubuntu@ip-172-31-4-252.us-east-2.compute.internal>
2024-11-22 22:14:36 +0000

    good stuff
    Signed-off-by: Ubuntu <ubuntu@ip-172-31-7-188.us-east-2.compute.internal>

---
+++ setup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#install Coach
+cd coach
+git submodule init
+git submodule update
+
+cd ..
+cp -f install.sh requirements_coach.txt ./coach/
+cd coach
+
+./install.sh
+
+# Install Other dependencies
+pip3 install gym==0.9.3
+pip3 install mujoco-py==0.5.7
+
+# Copy mujoco files
+cd ./../mujoco_files
+mkdir -p ~/.mujoco
+cp -r * ~/.mujoco/
+
+# sudo apt-get install nvidia-384
+# sudo apt-get install cuda-8-0
+
+pip install --ignore-installed box2d-py
+pip install --ignore-installed pachi-py
+
+pip install scipy==1.1.0 pandas==0.20.2 Pillow==8.4.0 pygame==1.9.3
+# pip3 install tensorflow==1.4.1 --ignore-installed
+pip3 install tensorflow-gpu==1.4.1 --ignore-installed
+pip install numpy==1.13.0
+
+conda update --all -y
+conda install -c conda-forge  glew glfw mesalib=23.0.0 -y
+
+
+#for installing cuda_8.0.61_375.26_linux-run    
+# export $PERL5LIB
